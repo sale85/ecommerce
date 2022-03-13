6cc77b8fe8fef7424729713cb0f31bb85c23c413
sale85 <101504892+sale85@users.noreply.github.com>
2022-03-13 12:22:29 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function open(){
+    document.getElementById("side-menu").style.display="block";
+    document.getElementById("menu-btn").style.display="none";
+    document.getElementById("close-btn").style.display="block";
+}
+function close(){
+    document.getElementById("side-menu").style.display="none";
+    document.getElementById("menu-btn").style.display="block";
+    document.getElementById("close-btn").style.display="none";
+}
